39db7234b9b517d7fbf3506e2c6cca4d638ff58f
SirvanD <sirvansh@gmail.com>
2022-03-12 14:12:33 +1100

    created movie card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import About from "./components/About/About.js";
+import MovieCard from "./components/MovieCard/MovieCard.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <MovieCard />
     </div>
   );
 }
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,5 +1,19 @@
+import style from "./MovieCard.module.scss";
 function Movie() {
-  return <div>Movie</div>;
+  return (
+    <div className={style.card}>
+      <img
+        className={style.poster}
+        src="https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
+        alt=""
+      />
+      <section className={style.movie_details}>
+        <h4>title</h4>
+        <h4>Cinema World:$ </h4>
+        <h4>Film World:$ </h4>
+      </section>
+    </div>
+  );
 }
 
 export default Movie;
