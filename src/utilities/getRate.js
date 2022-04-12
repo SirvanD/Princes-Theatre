82849be96404d3fa2661ebd96dd7021935e1c60c
SirvanD <sirvansh@gmail.com>
2022-04-12 12:40:34 +0430

    test 6

--- src/utilities/getRate.js
+++ src/utilities/getRate.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let AURate;
 //call API
-//test 5
+//test 6
 export default async function main() {
   const api = axios.create({
     baseURL: "https://challenge.lexicondigital.com.au/api/exchangerate/usd",
